ba514808120520bba25390ae1af5bb773f3008c7
snandi <nandi@stat.wisc.edu>
2014-12-01 15:48:42 -0600

    Updated some files in biostat acct

--- RScript6_Data1.R
+++ RScript6_Data1.R
@@ -10,17 +10,17 @@ rm(list=objects(all.names=TRUE))
 ########################################################################
 ## Run Header files                                                   ##
 ########################################################################
-Filename.Header <- paste('Z:/RScripts/HeaderFile_lmcg.R', sep='')
+Filename.Header <- paste('~/RScripts/HeaderFile_lmcg.R', sep='')
 source(Filename.Header)
 
-RScriptPath <- 'Z:/Courses/Stat760_Fall2014/Project/RScripts_Stat760/'
-RDataPath <- 'Z:/Courses/Stat760_Fall2014/Project/Data/'
+RScriptPath <- '~/Courses/Stat760_Fall2014/Project/RScripts_Stat760/'
+RDataPath <- '~/Courses/Stat760_Fall2014/Project/Data/'
 source(paste(RScriptPath, 'fn_Library_Project760.R', sep=''))
 
 ########################################################################
 ## Data 1                                                             ##
 ########################################################################
-Filename.Data1 <- 'Z:/Courses/Stat760_Fall2014/Project/Data/Data1_Leuk.txt'
+Filename.Data1 <- '~/Courses/Stat760_Fall2014/Project/Data/Data1_Leuk.txt'
 Data1 <- read.table(file=Filename.Data1, header=TRUE, sep='\t', quote="", 
                     comment.char="",
                     stringsAsFactors=F)
@@ -37,11 +37,11 @@ GMM_Output <- paste('Mclust recommends', Modelname, '(', mclustModelNames(Modeln
                     NumClusters, 'components')  
 print(GMM_Output)
 
-Filename.GMM1 <- 'Z:/Courses/Stat760_Fall2014/Project/Data/GMM1.RData'
+Filename.GMM1 <- '~/Courses/Stat760_Fall2014/Project/Data/GMM1.RData'
 save(GMM1, file=Filename.GMM1)
 
 Time1 <- Sys.time()
-Comparison1 <- clValid(obj=t(Data1[,-c(1:2)]), nClust=4:10, maxitems=30000,
+Comparison1 <- clValid(obj=Data1[,-c(1:2)], nClust=4:8, maxitems=30000,
                        clMethods=c("hierarchical", "kmeans", "pam"),
                        validation="internal")
 summary(Comparison1)
@@ -54,6 +54,6 @@ round(Measures[,,'kmeans'], 4)
 round(Measures[,,'pam'], 4)
 xtable(round(Measures[,,'hierarchical'], 4), digits = 4)
 
-Filename.Comp1 <- 'Z:/Courses/Stat760_Fall2014/Project/Data/Comparison1.RData'
+Filename.Comp1 <- '~/Courses/Stat760_Fall2014/Project/Data/Comparison1.RData'
 save(Comparison1, file=Filename.Comp1)
 
