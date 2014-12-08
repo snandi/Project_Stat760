ec9b0d72e43c976bd0b08ad0c5c665afffeebe10
snandi <nandi@stat.wisc.edu>
2014-12-07 18:11:22 -0600

    Updated clValid comparison and added ggplot code

--- RScript11_Data2_cValid.R
+++ RScript11_Data2_cValid.R
@@ -9,17 +9,18 @@ rm(list=objects(all.names=TRUE))
 ########################################################################
 ## Run Header files                                                   ##
 ########################################################################
-Filename.Header <- paste('~/RScripts/HeaderFile_lmcg.R', sep='')
+Filename.Header <- paste('Z:/RScripts/HeaderFile_lmcg.R', sep='')
 source(Filename.Header)
 
-RScriptPath <- '~/Courses/Stat760_Fall2014/Project/RScripts_Stat760/'
-RDataPath <- '~/Courses/Stat760_Fall2014/Project/Data/'
+RScriptPath <- 'Z:/Courses/Stat760_Fall2014/Project/RScripts_Stat760/'
+RDataPath <- 'Z:/Courses/Stat760_Fall2014/Project/Data/'
+RPlotPath <- 'Z:/Courses/Stat760_Fall2014/Project/RScripts_Stat760/Presentation/'
 source(paste(RScriptPath, 'fn_Library_Project760.R', sep=''))
 
 ########################################################################
 ## Load Data 2                                                        ##
 ########################################################################
-Filename.Data2_T <- '~/Courses/Stat760_Fall2014/Project/Data/Data2_Leuk_Transposed.txt'
+Filename.Data2_T <- 'Z:/Courses/Stat760_Fall2014/Project/Data/Data2_Leuk_Transposed.txt'
 Data2 <- read.table(file=Filename.Data2_T, header=TRUE, sep='\t', stringsAsFactors=F)
 #Data2.subset <- Data2[,1:10000]
 
@@ -32,18 +33,61 @@ NoL <- Data2[,49:60]
 ########################################################################
 ## clValid Comparison                                                 ##
 ########################################################################
-CompALL <- fn_runclValid(ALL, Name='ALL', DataPath=RDataPath)
-summary(CompALL)
+Time1 <- Sys.time()
+NumClusters=c(6:8)
+clMethods=c("hierarchical", "kmeans", "pam", "sota")
+Cluster_Type <- c(rep(x = 'hierarchical', times = length(NumClusters)),
+                  rep(x = 'kmeans', times = length(NumClusters)), 
+                  rep(x = 'pam', times = length(NumClusters)), 
+                  rep(x = 'sota', times = length(NumClusters)))
 
-CompAML <- fn_runclValid(AML, Name='AML', DataPath=RDataPath)
+CompALL <- fn_runclValid(Data = ALL[1:1000,], Name='ALL', DataPath=RDataPath, Validation = "internal", 
+                         NumClusters=NumClusters, clMethods=clMethods)
+print(Sys.time() - Time1)
+summary(CompALL)
+IndexComparison_ALL <- fn_returnIndexData(CompData = CompALL, NumClusters = NumClusters, 
+                                          Cluster_Type = Cluster_Type)
+Plot_ALL <- fn_plotComparisons(Data = IndexComparison_ALL, Cluster_Type = Cluster_Type)
+Plot_ALL
+dev.copy(device = jpeg, paste(RPlotPath, 'Plot_ALL.jpeg', sep=''))
+dev.off()
+
+Time1 <- Sys.time()
+CompAML <- fn_runclValid(Data = AML[1:1000,], Name='AML', DataPath=RDataPath, Validation = "internal", 
+                         NumClusters=NumClusters, clMethods=clMethods)
+print(Sys.time() - Time1)
 summary(CompAML)
+IndexComparison_AML <- fn_returnIndexData(CompData = CompAML, NumClusters = NumClusters, 
+                                          Cluster_Type = Cluster_Type)
+Plot_AML <- fn_plotComparisons(Data = IndexComparison_AML, Cluster_Type = Cluster_Type)
+Plot_AML
+dev.copy(device = jpeg, paste(RPlotPath, 'Plot_AML.jpeg', sep=''))
+dev.off()
 
 CompCLL <- fn_runclValid(CLL, Name='CLL', DataPath=RDataPath)
 summary(CompCLL)
+IndexComparison_CLL <- fn_returnIndexData(CompData = CompCLL, NumClusters = NumClusters, 
+                                          Cluster_Type = Cluster_Type)
+Plot_CLL <- fn_plotComparisons(Data = IndexComparison_CLL, Cluster_Type = Cluster_Type)
+Plot_CLL
+dev.copy(device = jpeg, paste(RPlotPath, 'Plot_AML.jpeg', sep=''))
+dev.off()
 
 CompCML <- fn_runclValid(CML, Name='CML', DataPath=RDataPath)
 summary(CompCML)
+IndexComparison_CML <- fn_returnIndexData(CompData = CompCML, NumClusters = NumClusters, 
+                                          Cluster_Type = Cluster_Type)
+Plot_CML <- fn_plotComparisons(Data = IndexComparison_CML, Cluster_Type = Cluster_Type)
+Plot_CML
+dev.copy(device = jpeg, paste(RPlotPath, 'Plot_CML.jpeg', sep=''))
+dev.off()
 
 CompNoL <- fn_runclValid(NoL, Name='NoL', DataPath=RDataPath)
 summary(CompNoL)
+IndexComparison_NoL <- fn_returnIndexData(CompData = CompNoL, NumClusters = NumClusters, 
+                                          Cluster_Type = Cluster_Type)
+Plot_NoL <- fn_plotComparisons(Data = IndexComparison_NoL, Cluster_Type = Cluster_Type)
+Plot_NoL
+dev.copy(device = jpeg, paste(RPlotPath, 'Plot_NoL.jpeg', sep=''))
+dev.off()
 
--- fn_Library_Project760.R
+++ fn_Library_Project760.R
@@ -52,13 +52,73 @@ fn_runGMM <- function(Data, Name='ALL', DataPath=RDataPath){
 }
 ########################################################################
 
-fn_runclValid <- function(Data, Name='ALL', DataPath=RDataPath){
-  Comparison <- clValid(obj=Data, nClust=4:8, maxitems=30000,
-                        clMethods=c("hierarchical", "kmeans", "pam"),
-                        validation="internal")
+fn_runclValid <- function(Data, Name='ALL', DataPath=RDataPath, Validation, NumClusters, clMethods){
+  Comparison <- clValid(obj=Data, nClust=NumClusters, maxitems=30000,
+                        clMethods=clMethods,
+                        validation=Validation)
   Name1 <- paste('Comparison', Name, sep='_')
   assign(x = Name1, value = Comparison)
   Filename <- paste(DataPath, Name1, '.RData', sep='')
   save(list = Name1, file=Filename)
   return(Comparison)
+}
+
+fn_returnIndexData <- function(CompData=CompALL, NumClusters, Cluster_Type){
+  Dunn <- c(measures(CompData, 'Dunn')[,,'hierarchical'], 
+            measures(CompData, 'Dunn')[,,'kmeans'], 
+            measures(CompData, 'Dunn')[,,'pam'], 
+            measures(CompData, 'Dunn')[,,'sota'])
+  Connectivity <- c(measures(CompData, 'Connectivity')[,,'hierarchical'], 
+                    measures(CompData, 'Connectivity')[,,'kmeans'], 
+                    measures(CompData, 'Connectivity')[,,'pam'], 
+                    measures(CompData, 'Connectivity')[,,'sota'])
+  Silhouette <- c(measures(CompData, 'Silhouette')[,,'hierarchical'], 
+                  measures(CompData, 'Silhouette')[,,'kmeans'], 
+                  measures(CompData, 'Silhouette')[,,'pam'], 
+                  measures(CompData, 'Silhouette')[,,'sota'])
+  Cluster_Num <- as.numeric(names(Dunn))
+
+  Cluster_Type <- c(rep(x = 'hierarchical', times = length(NumClusters)),
+                    rep(x = 'kmeans', times = length(NumClusters)), 
+                    rep(x = 'pam', times = length(NumClusters)), 
+                    rep(x = 'sota', times = length(NumClusters)))
+  
+  Measures <- as.data.frame(cbind(Cluster_Num, Dunn=as.vector(Dunn), 
+                                  Connectivity=as.vector(Connectivity), 
+                                  Silhouette=as.vector(Silhouette)))
+  
+  Measures <- within(data=Measures, {
+    Cluster_Num <- as.numeric(Cluster_Num)
+    Dunn <- as.numeric(Dunn)
+    Connectivity <- as.numeric(Connectivity)
+    Silhouette <- as.numeric(Silhouette)
+  })
+  
+  Data <- melt(data = Measures, id.vars = 'Cluster_Num')
+  
+  Data$Cluster_Type <- as.factor(rep(Cluster_Type, length(NumClusters)))
+  
+  Data <- plyr::rename(Data, c("variable"="Index", "value"="Index_Value"))
+  return(Data)
+}
+
+
+fn_plotComparisons <- function(Data, Cluster_Type){
+  Plot <- qplot(x=Cluster_Num, y=Index_Value, data=Data, color=Cluster_Type) + 
+    geom_line(size=1) + geom_point(size=4) + 
+    facet_wrap(~Index, ncol=1, scales="free") +
+    xlab(label = 'Cluster Size') + ylab('') + ggtitle('ALL: Acute Lymphoblastic Leukemia') +
+    theme(panel.background = element_rect(fill = 'black'), 
+          plot.background = element_rect(color='black', fill = "gray10"), 
+          plot.title=element_text(face="bold", size=15, colour="white"),
+          axis.title.x = element_text(colour = "white"), axis.title.y = element_text(colour = "white"), 
+          panel.grid.major = element_line(colour="gray20", size=0.35), 
+          panel.grid.minor = element_line(colour="gray10", size=0.25), 
+          legend.background = element_rect(fill = 'gray10'), 
+          legend.text = element_text(colour = "white", size=14),
+          legend.title = element_text(colour = "white", size=14), 
+          legend.position = 'bottom', 
+          legend.direction = 'horizontal'
+    )
+  return(Plot) 
 }
