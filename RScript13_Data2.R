9802b8a2d9e72ef64a93ae79b39a3fc5aa642650
snandi <nandi@stat.wisc.edu>
2014-12-10 23:24:02 -0600

    Updated Patient clustering script

--- RScript13_Data2.R
+++ RScript13_Data2.R
@@ -38,3 +38,34 @@ Comparison.Patients.Sc <- clValid(obj=Data2.T, nClust=4:8, maxitems=30000,
 summary(Comparison.Patients.Sc)
 
 GMM.Patients <- Mclust(data=t(Data2), G=4:8)
+summary(GMM.Patients)
+Filename.GMM <- paste(RDataPath, 'GMM_Patients.RData', sep='')
+save(GMM.Patients, file=Filename.GMM)
+
+BIC <- GMM.Patients$BIC
+BIC.r <- as.data.frame(melt(data = BIC))
+colnames(BIC.r) <- c('ClusterSize', 'ModelType', 'BIC')
+BIC.r <- within(data=BIC.r, {
+  ModelType <- as.factor(ModelType)
+})
+str(BIC.r)
+
+BICPlot <- qplot(x=ClusterSize, y=BIC, data=BIC.r, color=ModelType) +
+  geom_line(size=1) + geom_point(size=3) + 
+  xlab(label = 'Number of Clusters') + ylab('BIC') +
+  ggtitle('Cluster Size selection in Gaussian Mixture models') +
+  theme(panel.background = element_rect(fill = 'black'), 
+        plot.background = element_rect(color='black', fill = "gray10"), 
+        plot.title=element_text(face="bold", size=14, colour="white"),
+        axis.title.x = element_text(colour = "white"), axis.title.y = element_text(colour = "white"), 
+        panel.grid.major = element_line(colour="gray20", size=0.35), 
+        panel.grid.minor = element_line(colour="gray10", size=0.25), 
+        legend.background = element_rect(fill = 'gray10'), 
+        legend.text = element_text(colour = "white"),
+        legend.title = element_text(colour = "white"), 
+        legend.position = 'right'
+        #legend.direction = 'horizontal'
+  )   
+pdf(file=paste(RPlotPath, 'BICPlot_Patient.pdf', sep=''))
+BICPlot
+dev.off()
